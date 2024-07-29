4312724dff1e4d61ff6b52dc86335611b23d0a5d
RedaTargaoui <redatarg@gmail.com>
2024-07-29 14:39:27 +0200

    Update Main.java

--- src/main/java/com/supermarketcheckout/Main.java
+++ src/main/java/com/supermarketcheckout/Main.java
@@ -24,31 +24,12 @@ public class Main extends Application {
 
 		stage.setTitle("Supermarket Checkout");
 
-		stage.setOnCloseRequest(event -> {
-
-		});
-
 		stage.show();
 	}
 
 	public static void main(String[] args) {
 		// Check DATA dir existence:
 		DataHandler.checkDataDirExistence();
-
-		/*List<Product> products = new ArrayList<>();
-		products.add(new Product(1, "Apple", 0.99));
-		products.add(new Product(2, "Banana", 0.59));
-		products.add(new Product(3, "Orange", 1.29));
-
-		try {
-			DataHandler.writeProductsToFile(products);
-			List<Product> readProducts = DataHandler.readProductsFromFile();
-			for (Product product : readProducts) {
-				System.out.println(product.getId() +", " + product.getName() + ", " + product.getPrice());
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}*/
 
 		// Launch GUI:
 		launch();
